05e71715556c6ca3e933205ba8a903a0d83c6bf6
ABDALAZARD <abdalazard@gmail.com>
2023-05-09 16:12:37 -0300

    comenta blocos

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ export default function App() {
     const [valor, setValor] = useState(100)
     const [valor2, setValor2] = useState(100)
 
+    //O useState altera uma variavel sempre que for chamada, por exemplo por uma função
+    //O useEffect é alterado/renderizado uma vez, ou ao referenciar uma variavel e sempre que ela for alterada o useEffect é chamado
+    //ou com o segundo parametro vazio indicando a renderização durante a montagem da pagina
 
     function alterar(){
         setValor(valor + 10)
@@ -14,9 +17,19 @@ export default function App() {
         setValor2(valor2 + 10)
     }
 
+    //Essa função é executada quando a pagina for renderizada/montada
     useEffect(() => {
         console.log("alterado")
-    }, [])
+    }, []) 
+    //Nesta função só passaremos 1 ou 2 parametros: a função realizando a rotina do bloco
+    // e a variavel referenciando a alteração
+
+
+    //Essa função só é executada quando a variavel referenciada(valor) for alterada
+    useEffect(() => {
+        console.log("alterado")
+    }, [valor]) 
+
 
     return (        
         <div>
