dcebcba5eeab7b21fa50b72f360647f5531a5017
ABDALAZARD <abdalazard@gmail.com>
2023-05-09 16:29:03 -0300

    useEffect com arrays

--- src/App.js
+++ src/App.js
@@ -2,46 +2,49 @@ import React, { useState, useEffect } from 'react'
 
 export default function App() {
 
+    const clientes = ["João", "Ana", "Carlos"]
+    const produtos = ['Pão', 'Manteiga', 'Leite']
+    const vendas = ['1 x Pão (João)', '2 x Manteiga Ana', '3 x Leite (Carlos)']
+
+    const [info, setInfo] = useState("clientes")
     const [valor, setValor] = useState(100)
-    const [valor2, setValor2] = useState(100)
 
-    //O useState altera uma variavel sempre que for chamada, por exemplo por uma função
-    //O useEffect é alterado/renderizado uma vez, ou ao referenciar uma variavel e sempre que ela for alterada o useEffect é chamado
-    //ou com o segundo parametro vazio indicando a renderização durante a montagem da pagina
-
-    function alterar(){
-        setValor(valor + 10)
-    }
-
-    function alterar2(){
-        setValor2(valor2 + 10)
-    }
-
-    //Essa função é executada quando a pagina for renderizada/montada
     useEffect(() => {
-        console.log("alterado")
-    }, []) 
-    //Nesta função só passaremos 1 ou 2 parametros: a função realizando a rotina do bloco
-    // e a variavel referenciando a alteração
+        console.clear()
+        // eslint-disable-next-line default-case
+        switch (info) {
+            case 'clientes':
+                console.table(clientes)
+                break;
+            case 'produtos':
+                console.table(produtos)
+                break;
+            case 'vendas':
+                console.table(vendas)
+                break;
+        }
+    }, [info])
 
-
-    //Essa função só é executada quando a variavel referenciada(valor) for alterada
     useEffect(() => {
-        console.log("alterado")
-    }, [valor]) 
+        console.log('TESTE')
+    }, [valor])
 
 
-    return (        
+    return (
         <div>
             <h1>React Hooks - useEffect</h1>
             <hr />
-            <h3>Valor: {valor}</h3>
-            <h3>Valor 2: {valor2}</h3>
-            <button onClick={alterar}>Alterar</button>
-            <button onClick={alterar2}>Alterar 2</button>
 
-            
+            <button onClick={() => setInfo('clientes')}>Clientes</button>
+            <button onClick={() => setInfo('produtos')}>Produtos</button>
+            <button onClick={() => setInfo('vendas')}>Vendas</button>
+
+            <h3>{info}</h3>
+            <h2>Valor: {valor}</h2>
+
+            <button onClick={() => { setValor(valor + 10) }}>Aumentar</button>
+
         </div>
-        
+
     )
 }
