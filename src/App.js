191bfe010f9540b8a0dc518c141b1a1f85dc31e0
ABDALAZARD <abdalazard@gmail.com>
2023-05-11 16:20:20 -0300

    utiliza useMemo para armazenar função em memória e evitar má performace

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 
 export default function App() {
+    const [valor, setValor] = useState(() => { return 0 })
+    const [numero, setNumero] = useState(() => { return 1000 })
 
-     const [valor, setValor] = useState(0)
+    // const valorCalculado = funcaoDemorada(valor)
 
-     function decrementar(){
-        setValor(valor - 1)  
-     }
+    //Renderiza a cada interação da pagina
+    useEffect(() => {
+        console.log('render')
+    })
 
-     function incrementar(){
-        setValor(valor + 1)
-     }
+    //Essa função soma a const numero, e por isso não reage a demora
+    function incrementar1() {
+        setNumero((oldNumero) => oldNumero + 1)
+    }
 
-    return (        
+    //Essa função soma a const valor referenciada na função useMemo(), e por isso reage a demora
+    function incrementar2() {
+        setValor((oldValor) => oldValor + 1)
+    }
+
+    //Essa função executa uma demora de aproximadamente 1 segundo
+    function funcaoDemorada(num) {
+
+        for (let i = 0; i < 1000000000; i++) { }
+        return num * 2
+    }
+
+    //Essa função reage a cada alteração da const valor, ou seja, sempre que o botao "incrementar2"
+    //for clicado, o useMemo será executado já vinculando a demora a esta variavel
+    const valorCalculado = useMemo(() => {
+        return funcaoDemorada(valor)
+    }, [valor])
+
+    return (
         <div>
-            <h1>React Hooks</h1>
+            <h1>React Hooks - useMemo</h1>
             <p>Valor: {valor}</p>
-            <button onClick={decrementar}>Decrementar</button>
-            <button onClick={incrementar}>Incrementar</button>
+            <p>Numero: {numero}</p>
+            <p>Valor Calculado: {valorCalculado}</p>
+            <button onClick={incrementar1}>Incrementar</button>
+            <button onClick={incrementar2}>Incrementar 2</button>
         </div>
     )
 }
