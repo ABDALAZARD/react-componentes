30daa12f6406360e2b71e38fd36ccddcd3eaa7b9
ABDALAZARD <abdalazard@gmail.com>
2023-05-11 16:59:13 -0300

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 
 export default function App() {
 
+    //Reducer
+    //Essa função cria um objeto chamado state, no qual os valores são mostrados abaixo.
+    const [state, dispatch] = useReducer(reducer, {
+        valor: 1000,
+        mostrar: true
+    })
+
+    //função reducer
+    function reducer(state, action) {
+        switch (action.type) {
+            case 'INCREMENTAR':
+                return {
+                    valor: state.valor + 1,
+                    mostrar: state.mostrar
+                }
+            case 'VISIBILIDADE':
+                return {
+                    valor: state.valor,
+                    mostrar: !state.mostrar
+                }
+
+            default:
+                return state
+        }
+    }
 
     return (
         <div>
-            <h1>React</h1>
+            <h1>React Hooks - useReducer</h1>
+            <hr />
+            {state.mostrar && <p>Valor: {state.valor}</p>}
+            {state.mostrar && <p>Colocar Visível</p>}
+            <button onClick={() => { dispatch({ type: 'INCREMENTAR' }) }}>Incrementar</button>
+            <button onClick={() => { dispatch({ type: 'VISIBILIDADE' }) }}>Mostrar/Esconder</button>
         </div>
     )
 }
