28fc8c09c771e5f7208edb9bf101503d5acecad6
ABDALAZARD <abdalazard@gmail.com>
2023-05-11 16:21:01 -0300

    inicio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 export default function App() {
 
-     const [valor, setValor] = useState(0)
 
-     function decrementar(){
-        setValor(valor - 1)  
-     }
-
-     function incrementar(){
-        setValor(valor + 1)
-     }
-
-    return (        
+    return (
         <div>
-            <h1>React Hooks</h1>
-            <p>Valor: {valor}</p>
-            <button onClick={decrementar}>Decrementar</button>
-            <button onClick={incrementar}>Incrementar</button>
+            <h1>React</h1>
         </div>
     )
 }
