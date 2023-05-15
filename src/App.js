91d2e3d6993cd3df7bfd0ca213a6bc8457b0c5ce
ABDALAZARD <abdalazard@gmail.com>
2023-05-15 16:17:49 -0300

    Tabuada de multiplicação com react

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React, { useState } from 'react';
 
 export default function App() {
 
-    const [escuro, setEscuro] = useState(false)
+    const [valor, setValor] = useState(1)
+
+    const multiplicar = () => {
+        if (isNaN(valor)) return []
+        let produtos = []
+        for (let m = 1; m <= 10; m++) {
+            produtos.push(`${valor} x ${m} =  ${valor * m}`)
+        }
+        return produtos
+    }
+
     return (
-        <div style={{
-            backgroundColor: escuro ? "black" : "white",
-            color: escuro ? "white" : "black",
-            padding: 15
-        }}>
-            <h1>React - Tema Claro / Escuro</h1>
+        <div>
+            <h1>React - Tabuada</h1>
             <hr />
-            <button onClick={() => { setEscuro(old => !old) }}>Tema</button>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada sem volutpat ligula vestibulum, eget placerat turpis gravida. Maecenas id fermentum quam. Cras consequat tristique pharetra. Ut porttitor ullamcorper laoreet. Suspendisse cursus nunc eget justo pulvinar sagittis. Suspendisse magna nulla, interdum non hendrerit in, fringilla sit amet massa. Quisque a lacinia mauris, id ultrices lorem. Quisque eget porttitor tellus, in fermentum ipsum. Praesent sagittis eu tellus dapibus mattis. Pellentesque nec dolor ligula. Vivamus a sem ex. Proin aliquet, urna eget aliquam cursus, justo orci facilisis massa, eu finibus augue lectus quis velit. Nunc imperdiet massa magna, vitae suscipit enim sodales et. Sed ipsum nunc, commodo id convallis sollicitudin, rhoncus eget nibh. Fusce faucibus lectus risus, sit amet lacinia turpis rutrum in. In hac habitasse platea dictumst.</p>
+            <input type="number" onChange={(e) => { setValor(parseInt(e.target.value)) }} value={valor} />
+            {multiplicar().map(item => <p>{item}</p>)}
         </div>
     )
 }
