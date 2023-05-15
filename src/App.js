cc9b49458bc57306f11856d6765a8d9d3493a558
ABDALAZARD <abdalazard@gmail.com>
2023-05-15 16:09:10 -0300

    Mudando a cor do tempo do claro para escuro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React, { useState } from 'react';
-import Nomes from './Components/Nomes'
 
 export default function App() {
 
-    const [nome, setNome] = useState("")
-    const [nomes, setNomes] = useState([])
-
-    function guardarNome() {
-        setNomes(tmp => [...tmp, nome])
-    }
-
+    const [escuro, setEscuro] = useState(false)
     return (
-        <div>
-            <h3>React Unique Id</h3>
+        <div style={{
+            backgroundColor: escuro ? "black" : "white",
+            color: escuro ? "white" : "black",
+            padding: 15
+        }}>
+            <h1>React - Tema Claro / Escuro</h1>
             <hr />
-            <input type="text" onChange={e => setNome(e.target.value)} value={nome} />
-            <button onClick={guardarNome}>Guardar Nome</button>
-            <hr />
-            <Nomes nomes={nomes} />
+            <button onClick={() => { setEscuro(old => !old) }}>Tema</button>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada sem volutpat ligula vestibulum, eget placerat turpis gravida. Maecenas id fermentum quam. Cras consequat tristique pharetra. Ut porttitor ullamcorper laoreet. Suspendisse cursus nunc eget justo pulvinar sagittis. Suspendisse magna nulla, interdum non hendrerit in, fringilla sit amet massa. Quisque a lacinia mauris, id ultrices lorem. Quisque eget porttitor tellus, in fermentum ipsum. Praesent sagittis eu tellus dapibus mattis. Pellentesque nec dolor ligula. Vivamus a sem ex. Proin aliquet, urna eget aliquam cursus, justo orci facilisis massa, eu finibus augue lectus quis velit. Nunc imperdiet massa magna, vitae suscipit enim sodales et. Sed ipsum nunc, commodo id convallis sollicitudin, rhoncus eget nibh. Fusce faucibus lectus risus, sit amet lacinia turpis rutrum in. In hac habitasse platea dictumst.</p>
         </div>
     )
 }
