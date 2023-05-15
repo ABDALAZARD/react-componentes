6711987dcfdbea1112c3efdb5d5db580a01a6cf0
ABDALAZARD <abdalazard@gmail.com>
2023-05-15 16:42:04 -0300

    UseCallback para aumentar performace gravando função para ser chamada somente quando valor é alterado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
+import Lista from './Lista'
 
 export default function App() {
 
-    const [valor, setValor] = useState(1)
+    const [numero, setNumero] = useState(5)
+    const [temaEscuro, setTemaEscuro] = useState(false)
 
-    const multiplicar = () => {
-        if (isNaN(valor)) return []
-        let produtos = []
-        for (let m = 1; m <= 10; m++) {
-            produtos.push(`${valor} x ${m} =  ${valor * m}`)
-        }
-        return produtos
+    //Aumenta performace definindo que a função resultados
+    const resultados = useCallback(() => {
+        return [numero * 2, numero * 3, numero * 4]
+    }, [numero])
+
+    //tema do app
+    const tema = {
+        backgroundColor: temaEscuro ? "black" : "white",
+        color: temaEscuro ? "white" : "black",
+        height: "100vh"
     }
 
     return (
-        <div>
-            <h1>React - Tabuada</h1>
+        <div style={tema}>
+            <h1>React - useCallBack</h1>
             <hr />
-            <input type="number" onChange={(e) => { setValor(parseInt(e.target.value)) }} value={valor} />
-            {multiplicar().map(item => <p>{item}</p>)}
+            <button onClick={() => setTemaEscuro(!temaEscuro)}>Alterar Tema</button>
+            <p>Número: {numero}</p>
+            <button onClick={() => setNumero(oldNumero => oldNumero + 1)}>Incrementar</button>
+            <hr />
+            <Lista resultados={resultados} />
         </div>
     )
 }
