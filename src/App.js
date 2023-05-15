ce7aedbac15612dab8d20d607d2cc15799ddf7c7
ABDALAZARD <abdalazard@gmail.com>
2023-05-15 17:42:42 -0300

    Multiplos componente no mesmo arquivo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React, { useId } from 'react';
-
+import React from 'react';
+import { Componente1, Componente2, Componente3 } from './componentes.jsx'
 export default function App() {
-
-    const id1 = useId()
-    const id2 = useId()
 
     return (
         <div>
-            <h1>React - useId</h1>
+            <h2>React - Multiplos componentes no mesmo arquivo</h2>
             <hr />
-            <form action="#" method="post">
-                <div>
-                    <input type="checkbox" name="check_masculino" id={id1} />
-                    <label htmlFor={id1}>Texto da checkbox 1</label>
-                </div>
-                <div>
-                    <input type="checkbox" name="check_masculino" id={id2} />
-                    <label htmlFor={id2}>Texto da checkbox 2</label>
-                </div>
-            </form>
+            <Componente1 />
+            <Componente2 />
+            <Componente3 />
         </div>
     )
 }
